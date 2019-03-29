a6444ed87583d4421cb085383b4db40bbfa72f0e
DSAZ1324 <darteag94278@universidadean.edu.co>
2019-03-28 19:45:16 -0500

    Desarrollo funciones impares

--- adn.py
+++ adn.py
@@ -24,26 +24,51 @@ def generar_cadena_complementaria(adn):
     com = ''
     if int == type(adn):
         raise TypeError('No puede tener numeros')
+    if float == type(adn):
+        raise TypeError('No puede tener numeros')
     for letra in adn:
         com += obtener_complemento(letra)
     return com
 
 
 def calcular_correspondencia(adn1, adn2):
-    pass
+    complementorea = generar_cadena_complementaria(adn1)
+    print (complementorea)
+
+    total = len(adn2)
+    print(total)
+    coincidencias = 0
+    cont = 0
+    while (cont < total):
+        if(adn2[cont] == complementorea[cont]):
+            coincidencias = coincidencias + 1
+        cont = cont + 1
+        porcentaje = (coincidencias+100) / total
+    return porcentaje
 
 
 def corresponden(adn1, adn2):
+    if not es_cadena_valida(adn2):
+        raise ValueError(adn2 + ' no es una base')
     return generar_cadena_complementaria(adn1) == adn2
 
 
 def es_cadena_valida(adn):
-    if not es_base('T'):
-        return
-    return False
+    for letra in adn:
+        base = es_base(letra)
+        if base:
+            return True
+        if not base:
+            return False
 
 
 def es_base(caracter):
+    if int == type(caracter):
+        raise TypeError(str(caracter) + ' no es una base')
+    if float == type(caracter):
+        raise TypeError(str(caracter) + ' no es una base')
+    if len(caracter) != 1:
+        raise ValueError(caracter + ' tiene mas de 1 caracter')
     base = ['A', 'a', 'C', 'c', 'G', 'g', 'T', 't']
     if caracter in base:
         return True
@@ -56,6 +81,10 @@ def es_subcadena(adn1, adn2):
 
 
 def reparar_dano(adn, complementaria):
+    if int == type(complementaria):
+        raise TypeError('No puede tener numeros')
+    if not es_cadena_valida(complementaria):
+        raise ValueError(complementaria + ' no es una cadena valida')
     if corresponden(adn, complementaria):
         return "No presenta errores"
     elif not corresponden(adn, complementaria):
@@ -67,6 +96,7 @@ def obtener_secciones(adn, n):
 
 
 def obtener_complementos(lista_adn):
+
     com = []
     for cadena in lista_adn:
         com.append(generar_cadena_complementaria(cadena))
@@ -77,8 +107,9 @@ def unir_cadena(lista_adn):
     pass
 
 
-def complementar_cadenas(lista_adnn):
+def complementar_cadenas(lista_adn):
+
     com = ''
-    for cadena in lista_adnn:
+    for cadena in lista_adn:
         com += generar_cadena_complementaria(cadena)
     return com
